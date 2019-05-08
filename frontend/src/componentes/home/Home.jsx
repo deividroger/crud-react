034326c15fd706006d1c79c629901b752597306a
= <=>
2019-05-08 01:28:49 -0300

    first commit

---
+++ frontend/src/componentes/home/Home.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import Main from '../template/Main'
+
+export default props =>
+    <Main icon='home' subtitle='Cadastro de usuários'>
+        <div className='display-4'>Bem vindo</div>
+        <hr />
+        <p className='mb-a'>Sistema para exemplificar a contrução de um cadastro desenvolvido em React!</p>
+    </Main>
---
+++ frontend/src/componentes/template/Footer.jsx
@@ -0,0 +1,10 @@
+import './Footer.css'
+import React from 'react'
+
+export default props =>
+    <footer className='footer'>
+        <span>
+            Desenvolvido por <i className='fa fa-heart text-danger'></i> por
+            <strong> Deivid Roger</strong>
+        </span>
+    </footer>
